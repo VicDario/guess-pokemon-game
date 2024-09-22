1d2383c9b03aa0ce518897dbdc7c00889325cad1
VicDario <26585356+VicDario@users.noreply.github.com>
2024-09-22 20:00:34 -0300

    feat: show correct and incorrect options with colors

--- src/modules/pokemon/components/PokemonOptions.vue
+++ src/modules/pokemon/components/PokemonOptions.vue
@@ -4,7 +4,13 @@
       v-for="{ name, id } in options"
       :key="id"
       @click="$emit('selectedOption', id)"
-      class="capitalize disabled:shadow-none disabled:bg-gray-100"
+      :class="[
+        'capitalize disabled:shadow-none disabled:bg-gray-100',
+        {
+          correct: id === correctAnswer && blockSelection,
+          incorrect: id !== correctAnswer && blockSelection,
+        },
+      ]"
       :disabled="blockSelection"
     >
       {{ name }}
@@ -18,6 +24,7 @@ import type { Pokemon } from '../interfaces';
 interface Props {
   options: Pokemon[];
   blockSelection: boolean;
+  correctAnswer: number;
 }
 defineProps<Props>();
 defineEmits<{
@@ -28,5 +35,13 @@ defineEmits<{
 <style scoped>
 button {
   @apply bg-white shadow-md rounded-lg p-3 m-2 cursor-pointer w-40 text-center transition-all hover:bg-gray-100;
+}
+
+.correct {
+  @apply bg-blue-500 text-white hover:bg-blue-500;
+}
+
+.incorrect {
+  @apply bg-red-500 opacity-70 hover:bg-red-500;
 }
 </style>
