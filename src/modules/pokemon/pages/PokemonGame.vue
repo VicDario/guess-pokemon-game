61d10aff54bc6365e1233f6921a1e054f83b2bb1
VicDario <26585356+VicDario@users.noreply.github.com>
2024-09-22 18:34:58 -0300

    refactor: use computed ref to avoid duplicated code

--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -6,23 +6,22 @@
 
   <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">¿Who's that pokemon?</h1>
-    <PokemonPicture
-      :pokemon-id="randomPokemon.id"
-      :show-pokemon="gameStatus !== GameStatus.Playing"
-    />
+    <PokemonPicture :pokemon-id="randomPokemon.id" :show-pokemon="!isPlaying" />
     <PokemonOptions
       :options="pokemonOptions"
-      :block-selection="gameStatus !== GameStatus.Playing"
+      :block-selection="!isPlaying"
       @selected-option="checkAnswer"
     />
   </section>
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue';
 import PokemonOptions from '../components/PokemonOptions.vue';
 import PokemonPicture from '../components/PokemonPicture.vue';
 import { usePokemonGame } from '../composables/usePokemonGame';
 import { GameStatus } from '../interfaces';
 
+const isPlaying = computed<boolean>(() => gameStatus.value === GameStatus.Playing);
 const { randomPokemon, gameStatus, isLoading, pokemonOptions, checkAnswer } = usePokemonGame();
 </script>
