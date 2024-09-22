e17b9ce8eb3fd6e761c7c307df3d5b93e41c0c9c
VicDario <26585356+VicDario@users.noreply.github.com>
2024-09-21 23:05:13 -0300

    feat: add pokemon picture component

---
+++ src/modules/pokemon/components/PokemonPicture.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <img :src="image" alt="Pokémon" class="brightness-0 h-[200px]" />
+  </section>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  image: string;
+}
+defineProps<Props>();
+</script>
+
+<style scoped>
+img {
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-select: none;
+  -webkit-user-drag: none;
+}
+</style>
