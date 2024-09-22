3eca3401d74394b88b1dd231507540ab11604bfa
VicDario <26585356+VicDario@users.noreply.github.com>
2024-09-22 18:36:06 -0300

    refactor: better name

--- src/modules/pokemon/composables/usePokemonGame.ts
+++ src/modules/pokemon/composables/usePokemonGame.ts
@@ -27,7 +27,7 @@ export const usePokemonGame = () => {
     return pokemonsArray.sort(() => Math.random() - 0.5);
   };
 
-  const getNextOptions = (howMany: number = 4) => {
+  const getNextRound = (howMany: number = 4) => {
     gameStatus.value = GameStatus.Playing;
     pokemonOptions.value = pokemons.value.slice(0, howMany);
     pokemons.value = pokemons.value.slice(howMany);
@@ -47,7 +47,7 @@ export const usePokemonGame = () => {
 
   onMounted(async () => {
     pokemons.value = await getPokemons();
-    getNextOptions();
+    getNextRound();
   });
 
   return {
@@ -57,7 +57,7 @@ export const usePokemonGame = () => {
     randomPokemon,
 
     // Methods
-    getNextOptions,
+    getNextRound,
     checkAnswer,
   };
 };
